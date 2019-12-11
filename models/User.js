487a1db229b0a411187546331bd4332809ba2d69
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2019-12-10 22:17:28 -0600

    Made a server folder and User schema

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: "Email address is required.",
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    default: "Guest"
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
