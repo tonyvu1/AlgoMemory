9231eba2143d8c32004f8ed6ceeab9e130be6bd8
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2019-12-10 22:25:05 -0600

    Made users route

---
+++ server/routes/users.js
@@ -0,0 +1,24 @@
+const router = require("express");
+
+let User = require("../models/User");
+
+router.route("/").get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json(`Error: ` + err));
+});
+
+router.route("/add").post((req, res) => {
+  const email = req.body.email;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+
+  const newUser = User({ email, firstName, lastName });
+
+  newUser
+    .save()
+    .then(() => res.json("User added"))
+    .catch(err => res.status(400).json(`Error: ` + err));
+});
+
+module.exports = router;
