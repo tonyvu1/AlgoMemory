a396b0d873eda023a8b6fd82aab257d67b4dc30c
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2019-12-19 16:34:48 -0600

    setting up deployment

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ app.use(express.json());
 const usersRouter = require("./server/routes/users");
 app.use("/users", usersRouter);
 
+/***************************** HANDLE PRODUCTION *******************************/
+if (process.env.NODE_ENV === "production") {
+  // Static folder
+  app.use(express.static(__dirname + "/public"));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
