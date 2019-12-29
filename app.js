f7a2271ed08a4e46fb4b9ca62588372df3ceab44
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2019-12-28 22:04:53 -0600

    test deploy

--- app.js
+++ app.js
@@ -24,16 +24,24 @@ connection.once("open", () => {
 app.use(cors());
 app.use(express.json());
 
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Routes
 const usersRouter = require("./server/routes/users");
 app.use("/users", usersRouter);
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('invaild endpoint');
+});
 
 /***************************** HANDLE PRODUCTION *******************************/
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server started on port: ${PORT}`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
 });
