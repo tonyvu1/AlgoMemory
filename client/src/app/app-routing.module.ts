8d291cfbf2b584a5f1fb6a5d31e5cc2f22f4bfcc
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2020-01-05 13:32:27 -0600

    implemented auth guard for protected routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { StringReversalComponent } from "./components/algorithms/string-reversal
 import { BubbleSortComponent } from "./components/algorithms/bubble-sort/bubble-sort.component";
 import { SelectionSortComponent } from "./components/algorithms/selection-sort/selection-sort.component";
 import { ProfileComponent } from "./components/profile/profile.component"
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "string-reversal", component: StringReversalComponent },
   { path: "bubble-sort", component: BubbleSortComponent },
   { path: "selection-sort", component: SelectionSortComponent },
-  { path: "profile", component: ProfileComponent }
+  { path: "profile", component: ProfileComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
